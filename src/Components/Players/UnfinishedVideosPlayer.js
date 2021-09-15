4a01f549e600041f2fcbf7db0bee8bc4b32f72d2
Rakesh <iamrakeshkumar@pm.me>
2021-09-15 11:51:22 +0530

    fix:limiting unfinshed videos

--- src/Components/Players/UnfinishedVideosPlayer.js
+++ src/Components/Players/UnfinishedVideosPlayer.js
@@ -8,33 +8,36 @@ export default function UnfinishedVideosPlayer({
   return (
     <div className="unfinished-videos">
       <div className="flex-row ">
-        {unfinishedVideos.map(({ video, elapsedTime, remainingTime }) => (
-          <div className="flex-col-lg-4 p-one" key={video._id}>
-            <div className="small-video-card flex">
-              <div className="small-card-img">
-                <img
-                  src={`https://img.youtube.com/vi/${video.youtubeId}/mqdefault.jpg`}
-                  alt=""
-                />
-              </div>
-              <div className="small-card-desc flex-dir-col">
-                <span
-                  className="small-card-title m-bottom pointer"
-                  onClick={() =>
-                    watchUnfinishedHandeller(elapsedTime, video._id)
-                  }
-                >
-                  {video.name}
-                </span>
-                <span className="small-card-time">
-                  {remainingTime <= 0
-                    ? "less than 1 mintue left"
-                    : `${remainingTime} minute left`}
-                </span>
+        {[...unfinishedVideos]
+          .reverse()
+          .slice(0, 3)
+          .map(({ video, elapsedTime, remainingTime }) => (
+            <div className="flex-col-lg-4 p-one" key={video._id}>
+              <div className="small-video-card flex">
+                <div className="small-card-img">
+                  <img
+                    src={`https://img.youtube.com/vi/${video.youtubeId}/mqdefault.jpg`}
+                    alt=""
+                  />
+                </div>
+                <div className="small-card-desc flex-dir-col">
+                  <span
+                    className="small-card-title m-bottom pointer"
+                    onClick={() =>
+                      watchUnfinishedHandeller(elapsedTime, video._id)
+                    }
+                  >
+                    {video.name}
+                  </span>
+                  <span className="small-card-time">
+                    {remainingTime <= 0
+                      ? "less than 1 mintue left"
+                      : `${remainingTime} minute left`}
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
